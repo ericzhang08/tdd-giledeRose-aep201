d2ff0bef12bc5635b554aa36470a8c3bad17e361
ericzhang08 <yuzhangfish@163.com>
2020-06-09 17:51:42 +0800

    should_quality_and_sellIn_minus_one_when_one_day_passed_given_normal_product_on_date_one_day

--- src/test/java/tdd/giledeRose/aep201/NormalProductTest.java
+++ src/test/java/tdd/giledeRose/aep201/NormalProductTest.java
@@ -13,4 +13,12 @@ public class NormalProductTest {
         assertEquals( 4, normalProduct.getSellIn());
         assertEquals(9, normalProduct.getQuality());
     }
+
+    @Test
+    public void should_quality_and_sellIn_minus_one_when_one_day_passed_given_normal_product_on_date_one_day() {
+        NormalProduct normalProduct = new NormalProduct(1, 10);
+        normalProduct.oneDayPassed();
+        assertEquals( 0, normalProduct.getSellIn());
+        assertEquals(9, normalProduct.getQuality());
+    }
 }
